395faad909c9a4c07bca05aac9d4dd67d70cc4df
时雨 <270598250@qq.com>
2026-05-01 11:46:08 +0800

    chore(d3d12): implement resource objects

--- src/d3d12/d3d12_resource.cpp
+++ src/d3d12/d3d12_resource.cpp
@@ -1,3 +1,284 @@
 #include "d3d12_resource.hpp"
 
-// Implementation intentionally omitted in this scaffold-only step.
+#include "com/com_guid.hpp"
+#include "com/com_object.hpp"
+#include "com/com_private_data.hpp"
+#include "d3d12_heap.hpp"
+#include "dxmt_format.hpp"
+#include "log/log.hpp"
+#include "util_string.hpp"
+#include <algorithm>
+#include <cstring>
+
+namespace dxmt::d3d12 {
+namespace {
+
+WMTTextureUsage
+GetTextureUsage(D3D12_RESOURCE_FLAGS flags) {
+  WMTTextureUsage usage = WMTTextureUsageShaderRead;
+  if (flags & (D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET |
+               D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL))
+    usage |= WMTTextureUsageRenderTarget;
+  if (flags & D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS)
+    usage |= WMTTextureUsageShaderRead | WMTTextureUsageShaderWrite;
+  return usage;
+}
+
+WMTTextureType
+GetTextureType(const D3D12_RESOURCE_DESC &desc) {
+  if (desc.SampleDesc.Count > 1)
+    return desc.DepthOrArraySize > 1 ? WMTTextureType2DMultisampleArray
+                                     : WMTTextureType2DMultisample;
+  return desc.DepthOrArraySize > 1 ? WMTTextureType2DArray : WMTTextureType2D;
+}
+
+class ResourceImpl final : public ComObjectWithInitialRef<ID3D12Resource>,
+                           public Resource {
+public:
+  ResourceImpl(IMTLD3D12Device *device,
+               const D3D12_HEAP_PROPERTIES &heap_properties,
+               D3D12_HEAP_FLAGS heap_flags,
+               const D3D12_RESOURCE_DESC &desc,
+               D3D12_RESOURCE_STATES initial_state,
+               UINT64 heap_offset)
+      : device_(device), heap_properties_(heap_properties),
+        heap_flags_(heap_flags), desc_(desc), initial_state_(initial_state),
+        heap_offset_(heap_offset) {
+    if (desc_.Dimension == D3D12_RESOURCE_DIMENSION_BUFFER)
+      CreateBuffer();
+    else
+      CreateTexture2D();
+  }
+
+  HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid,
+                                           void **ppvObject) override {
+    if (!ppvObject)
+      return E_POINTER;
+
+    *ppvObject = nullptr;
+    if (riid == __uuidof(IUnknown) || riid == __uuidof(ID3D12Object) ||
+        riid == __uuidof(ID3D12DeviceChild) ||
+        riid == __uuidof(ID3D12Pageable) || riid == __uuidof(ID3D12Resource)) {
+      *ppvObject = ref(this);
+      return S_OK;
+    }
+
+    if (logQueryInterfaceError(__uuidof(ID3D12Resource), riid))
+      WARN("D3D12Resource: unknown interface query ", str::format(riid));
+    return E_NOINTERFACE;
+  }
+
+  HRESULT STDMETHODCALLTYPE GetPrivateData(REFGUID guid, UINT *data_size,
+                                           void *data) override {
+    return private_data_.getData(guid, data_size, data);
+  }
+
+  HRESULT STDMETHODCALLTYPE SetPrivateData(REFGUID guid, UINT data_size,
+                                           const void *data) override {
+    return private_data_.setData(guid, data_size, data);
+  }
+
+  HRESULT STDMETHODCALLTYPE SetPrivateDataInterface(REFGUID guid,
+                                                   const IUnknown *data) override {
+    return private_data_.setInterface(guid, data);
+  }
+
+  HRESULT STDMETHODCALLTYPE SetName(const WCHAR *name) override {
+    name_ = name ? str::fromws(name) : std::string();
+    return S_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE GetDevice(REFIID riid, void **device) override {
+    return device_->QueryInterface(riid, device);
+  }
+
+  HRESULT STDMETHODCALLTYPE Map(UINT sub_resource, const D3D12_RANGE *read_range,
+                                void **data) override {
+    if (!data)
+      return E_POINTER;
+    *data = nullptr;
+
+    if (desc_.Dimension != D3D12_RESOURCE_DIMENSION_BUFFER || sub_resource != 0)
+      return E_INVALIDARG;
+    if (!buffer_allocation_ || !buffer_allocation_->mappedMemory(0))
+      return E_INVALIDARG;
+
+    *data = static_cast<char *>(buffer_allocation_->mappedMemory(0)) +
+            heap_offset_;
+    return S_OK;
+  }
+
+  void STDMETHODCALLTYPE Unmap(UINT sub_resource,
+                               const D3D12_RANGE *written_range) override {
+    if (desc_.Dimension != D3D12_RESOURCE_DIMENSION_BUFFER ||
+        sub_resource != 0 || !buffer_allocation_)
+      return;
+
+    if (written_range && written_range->End > written_range->Begin)
+      buffer_allocation_->flushCpuShadow(written_range->Begin,
+                                         written_range->End - written_range->Begin);
+  }
+
+#ifdef WIDL_EXPLICIT_AGGREGATE_RETURNS
+  D3D12_RESOURCE_DESC *STDMETHODCALLTYPE
+  GetDesc(D3D12_RESOURCE_DESC *__ret) override {
+    *__ret = desc_;
+    return __ret;
+  }
+#else
+  D3D12_RESOURCE_DESC STDMETHODCALLTYPE GetDesc() override {
+    return desc_;
+  }
+#endif
+
+  D3D12_GPU_VIRTUAL_ADDRESS STDMETHODCALLTYPE
+  GetGPUVirtualAddress() override {
+    if (desc_.Dimension != D3D12_RESOURCE_DIMENSION_BUFFER ||
+        !buffer_allocation_)
+      return 0;
+    return buffer_allocation_->gpuAddress() + heap_offset_;
+  }
+
+  HRESULT STDMETHODCALLTYPE WriteToSubresource(
+      UINT dst_sub_resource, const D3D12_BOX *dst_box, const void *src_data,
+      UINT src_row_pitch, UINT src_slice_pitch) override {
+    return E_NOTIMPL;
+  }
+
+  HRESULT STDMETHODCALLTYPE ReadFromSubresource(
+      void *dst_data, UINT dst_row_pitch, UINT dst_slice_pitch,
+      UINT src_sub_resource, const D3D12_BOX *src_box) override {
+    return E_NOTIMPL;
+  }
+
+  HRESULT STDMETHODCALLTYPE
+  GetHeapProperties(D3D12_HEAP_PROPERTIES *heap_properties,
+                    D3D12_HEAP_FLAGS *flags) override {
+    if (!heap_properties || !flags)
+      return E_POINTER;
+    *heap_properties = heap_properties_;
+    *flags = heap_flags_;
+    return S_OK;
+  }
+
+  const D3D12_RESOURCE_DESC &GetResourceDesc() const override {
+    return desc_;
+  }
+
+  const D3D12_HEAP_PROPERTIES &GetResourceHeapProperties() const override {
+    return heap_properties_;
+  }
+
+  D3D12_HEAP_FLAGS GetResourceHeapFlags() const override {
+    return heap_flags_;
+  }
+
+  UINT64 GetHeapOffset() const override {
+    return heap_offset_;
+  }
+
+  D3D12_RESOURCE_STATES GetInitialState() const override {
+    return initial_state_;
+  }
+
+  dxmt::Buffer *GetBuffer() const override {
+    return buffer_.ptr();
+  }
+
+  dxmt::BufferAllocation *GetBufferAllocation() const override {
+    return buffer_allocation_.ptr();
+  }
+
+  dxmt::Texture *GetTexture() const override {
+    return texture_.ptr();
+  }
+
+  dxmt::TextureAllocation *GetTextureAllocation() const override {
+    return texture_allocation_.ptr();
+  }
+
+private:
+  void CreateBuffer() {
+    buffer_ = new dxmt::Buffer(desc_.Width + heap_offset_,
+                               device_->GetDXMTDevice().device());
+    buffer_allocation_ =
+        buffer_->allocate(GetHeapBufferAllocationFlags(heap_properties_));
+  }
+
+  void CreateTexture2D() {
+    MTL_DXGI_FORMAT_DESC format = {};
+    if (FAILED(MTLQueryDXGIFormat(device_->GetDXMTDevice().device(),
+                                  desc_.Format, format)))
+      return;
+
+    WMTTextureInfo info = {};
+    info.pixel_format = format.PixelFormat;
+    info.width = static_cast<uint32_t>(desc_.Width);
+    info.height = desc_.Height;
+    info.depth = 1;
+    info.array_length = desc_.DepthOrArraySize;
+    info.type = GetTextureType(desc_);
+    info.mipmap_level_count = desc_.MipLevels ? desc_.MipLevels : 1;
+    info.sample_count = desc_.SampleDesc.Count ? desc_.SampleDesc.Count : 1;
+    info.usage = GetTextureUsage(desc_.Flags);
+
+    texture_ = new dxmt::Texture(info, device_->GetDXMTDevice().device());
+    Flags<dxmt::TextureAllocationFlag> flags;
+    if (GetHeapType(heap_properties_) == D3D12_HEAP_TYPE_DEFAULT) {
+      flags.set(dxmt::TextureAllocationFlag::CpuInvisible);
+      flags.set(dxmt::TextureAllocationFlag::GpuPrivate);
+    } else if (GetHeapType(heap_properties_) == D3D12_HEAP_TYPE_UPLOAD) {
+      flags.set(dxmt::TextureAllocationFlag::CpuWriteCombined);
+    }
+    texture_allocation_ = texture_->allocate(flags);
+  }
+
+  Com<IMTLD3D12Device> device_;
+  ComPrivateData private_data_;
+  D3D12_HEAP_PROPERTIES heap_properties_ = {};
+  D3D12_HEAP_FLAGS heap_flags_ = D3D12_HEAP_FLAG_NONE;
+  D3D12_RESOURCE_DESC desc_ = {};
+  D3D12_RESOURCE_STATES initial_state_ = D3D12_RESOURCE_STATE_COMMON;
+  UINT64 heap_offset_ = 0;
+  Rc<dxmt::Buffer> buffer_;
+  Rc<dxmt::BufferAllocation> buffer_allocation_;
+  Rc<dxmt::Texture> texture_;
+  Rc<dxmt::TextureAllocation> texture_allocation_;
+  std::string name_;
+};
+
+} // namespace
+
+bool
+IsSupportedResourceDesc(const D3D12_RESOURCE_DESC &desc) {
+  if (desc.Width == 0 || desc.Height == 0 || desc.DepthOrArraySize == 0)
+    return false;
+  if (desc.SampleDesc.Count == 0)
+    return false;
+  if (desc.Dimension == D3D12_RESOURCE_DIMENSION_BUFFER) {
+    return desc.Height == 1 && desc.DepthOrArraySize == 1 &&
+           desc.MipLevels == 1 && desc.Format == DXGI_FORMAT_UNKNOWN &&
+           desc.SampleDesc.Count == 1 && desc.SampleDesc.Quality == 0 &&
+           desc.Layout == D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+  }
+  if (desc.Dimension == D3D12_RESOURCE_DIMENSION_TEXTURE2D) {
+    if (desc.Layout != D3D12_TEXTURE_LAYOUT_UNKNOWN &&
+        desc.Layout != D3D12_TEXTURE_LAYOUT_64KB_UNDEFINED_SWIZZLE &&
+        desc.Layout != D3D12_TEXTURE_LAYOUT_64KB_STANDARD_SWIZZLE)
+      return false;
+    return desc.Format != DXGI_FORMAT_UNKNOWN;
+  }
+  return false;
+}
+
+Com<ID3D12Resource>
+CreateResource(IMTLD3D12Device *device,
+               const D3D12_HEAP_PROPERTIES *heap_properties,
+               D3D12_HEAP_FLAGS heap_flags, const D3D12_RESOURCE_DESC *desc,
+               D3D12_RESOURCE_STATES initial_state, UINT64 heap_offset) {
+  return Com<ID3D12Resource>::transfer(
+      new ResourceImpl(device, *heap_properties, heap_flags, *desc,
+                       initial_state, heap_offset));
+}
+
+} // namespace dxmt::d3d12
--- src/d3d12/d3d12_resource.hpp
+++ src/d3d12/d3d12_resource.hpp
@@ -1,12 +1,34 @@
 #pragma once
 
+#include "d3d12_device.hpp"
 #include "dxmt_buffer.hpp"
 #include "dxmt_texture.hpp"
 #include "com/com_pointer.hpp"
+#include "rc/util_rc_ptr.hpp"
 #include <d3d12.h>
 
 namespace dxmt::d3d12 {
 
-class Resource;
+class Resource {
+public:
+  virtual ~Resource() = default;
+
+  virtual const D3D12_RESOURCE_DESC &GetResourceDesc() const = 0;
+  virtual const D3D12_HEAP_PROPERTIES &GetResourceHeapProperties() const = 0;
+  virtual D3D12_HEAP_FLAGS GetResourceHeapFlags() const = 0;
+  virtual UINT64 GetHeapOffset() const = 0;
+  virtual D3D12_RESOURCE_STATES GetInitialState() const = 0;
+  virtual dxmt::Buffer *GetBuffer() const = 0;
+  virtual dxmt::BufferAllocation *GetBufferAllocation() const = 0;
+  virtual dxmt::Texture *GetTexture() const = 0;
+  virtual dxmt::TextureAllocation *GetTextureAllocation() const = 0;
+};
+
+Com<ID3D12Resource>
+CreateResource(IMTLD3D12Device *device, const D3D12_HEAP_PROPERTIES *heap_properties,
+               D3D12_HEAP_FLAGS heap_flags, const D3D12_RESOURCE_DESC *desc,
+               D3D12_RESOURCE_STATES initial_state, UINT64 heap_offset);
+
+bool IsSupportedResourceDesc(const D3D12_RESOURCE_DESC &desc);
 
 } // namespace dxmt::d3d12
